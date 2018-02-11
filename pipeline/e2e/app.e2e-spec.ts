a56a0a691122f1c43940de0cceb261e02f2eb85a
Alan Tai <Alan.Tai@riverbed.com>
2018-02-11 12:05:48 -0800

    fix unit test and e2e test

--- pipeline/e2e/app.e2e-spec.ts
+++ pipeline/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('pipeline App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Let\'s Go with Angular on Dock');
   });
 });
--- pipeline/src/app/app.component.ts
+++ pipeline/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, Inject } from '@angular/core';
+import { Component, AfterViewInit, Inject, NgZone } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 import * as WaveSurfer from 'wavesurfer';
@@ -12,9 +12,10 @@ import * as THREE from 'three';
 export class AppComponent implements AfterViewInit {
   title = 'Let\'s Go with Angular on Dock';
   btnPlayOrPause = 'Play';
-  wavesurfer = null;
+  wavesurfer: any = null;
 
-  constructor(@Inject(DOCUMENT) private document: any) {}
+  constructor(@Inject(DOCUMENT) private document: any,
+    private zone: NgZone) {}
 
   playPause() {
     if (this.wavesurfer) this.wavesurfer.playPause();
@@ -23,60 +24,62 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     const _this = this;
-    // init wavesurfer
-    this.wavesurfer = WaveSurfer.create({
-      container: '#waveform',
-      waveColor: 'red',
-      progressColor: 'purple'
+
+    this.zone.runOutsideAngular(() => {
+      // init wavesurfer
+      _this.wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'red',
+        progressColor: 'purple'
+      });
+
+      // load mp3
+      _this.wavesurfer.load('/assets/tez_cadey_seve.mp3');
+      _this.wavesurfer.on('finish', function () {
+        _this.btnPlayOrPause = 'Play'
+      });
+
+
+      // three.js
+      let scene = new THREE.Scene();
+      let W = window.innerWidth;
+      let H = window.innerHeight;
+
+      let renderer = new THREE.WebGLRenderer();
+      renderer.setClearColor(0xffffff);
+      renderer.setSize(W, H);
+
+      let camera = new THREE.PerspectiveCamera(60, W / H, 0.3, 10000);
+
+      let planeGeometry = new THREE.PlaneGeometry(1000, 600, 200, 100);
+      let planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+      let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+      plane.rotation.x = -0.2 * Math.PI;
+
+      plane.position.set(0, 0, 0);
+
+      scene.add(plane);
+
+      camera.position.set(0, 50, 100);
+      camera.lookAt(scene.position);
+
+      document.body.appendChild(renderer.domElement);
+
+      (function drawFrame(ts: number){
+        let center = new THREE.Vector2(0,0);
+        window.requestAnimationFrame(drawFrame);
+        let vLength = (plane as any).geometry.vertices.length;
+        for (let i = 0; i < vLength; i++) {
+          let v = (plane as any).geometry.vertices[i];
+          let dist = new THREE.Vector2(v.x, v.y).sub(center);
+          let size = 5.0;
+          let magnitude = 4;
+          v.z = Math.sin(dist.length()/-size + (ts/100)) * magnitude;
+        }
+        (plane as any).geometry.verticesNeedUpdate = true;
+        renderer.render(scene, camera);
+      }(1));
     });
-
-    // load mp3
-    this.wavesurfer.load('/assets/tez_cadey_seve.mp3');
-    this.wavesurfer.on('finish', function () {
-      _this.btnPlayOrPause = 'Play'
-    });
-
-
-    // three.js
-    let scene = new THREE.Scene();
-
-    let W = window.innerWidth;
-    let H = window.innerHeight;
-
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xffffff);
-    renderer.setSize(W, H);
-
-    let camera = new THREE.PerspectiveCamera(60, W / H, 0.3, 10000);
-
-    let planeGeometry = new THREE.PlaneGeometry(1000, 600, 200, 100);
-    let planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-    plane.rotation.x = -0.2 * Math.PI;
-
-    plane.position.set(0, 0, 0);
-
-    scene.add(plane);
-
-    camera.position.set(0, 50, 100);
-    camera.lookAt(scene.position);
-
-    document.body.appendChild(renderer.domElement);
-
-    (function drawFrame(ts){
-      let center = new THREE.Vector2(0,0);
-      window.requestAnimationFrame(drawFrame);
-      let vLength = (plane as any).geometry.vertices.length;
-      for (let i = 0; i < vLength; i++) {
-        let v = (plane as any).geometry.vertices[i];
-        let dist = new THREE.Vector2(v.x, v.y).sub(center);
-        let size = 5.0;
-        let magnitude = 4;
-        v.z = Math.sin(dist.length()/-size + (ts/100)) * magnitude;
-      }
-      (plane as any).geometry.verticesNeedUpdate = true;
-      renderer.render(scene, camera);
-    }());
   }
 }
